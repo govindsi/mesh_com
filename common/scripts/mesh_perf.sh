cfb23cfce6d89d74c28f82db18f31fa9722de80d
Govind Singh <govind.sk85@gmail.com>
2021-06-23 21:00:21 +0530

    mesh_com: Add conf file support for perf configuration
    Add conf file for some of the wifi radio perf parameter.
    
    Example config:mesh_perf.conf
    
    ps_disable = 1
    tx_power = 20000
    distance = 6000
    coverage_class = 1
    
    Signed-off-by: Govind Singh <govind.sk85@gmail.com>
    Signed-off-by: Govind Singh <govind@ssrc.tii.ae>

--- common/scripts/mesh_perf.sh
+++ common/scripts/mesh_perf.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 
-disable_wifi_powersave()
+apply_radio_config()
 {
 test -f /etc/network/if-up.d/mesh_if_ps | exit
 
@@ -24,7 +24,9 @@ if [ "\$ADDRFAM" != inet ]; then
 fi
 
 /sbin/iw dev $MESH_IF set power_save off
-
+/sbin/iw phy $phy_if set txpower fixed $tx_power
+/sbin/iw phy $phy_if set distance $distance
+/sbin/iw phy $phy_if set coverage $coverage_class
 exit 0
 EOF
 
@@ -32,59 +34,66 @@ EOF
 
 help()
 {
-  echo "sudo ./mesh_perf.sh -i wlan0 -p 1 -l value"
+  echo "sudo ./mesh_perf.sh -i wlan0 -l value"
 }
 
-while getopts ":i:p:l" option; do
+while getopts ":i:l" option; do
   case $option in
     i)
       MESH_IF=${OPTARG};;
-    p)
-      power_save=${OPTARG};;
     l)
       latency_mode=${OPTARG};;
   esac
 
 done
 
+CONF_FILE="mesh_perf.conf"
+ps_disable=$(awk '/^ps_disable/{print $3}' $CONF_FILE)
+tx_power=$(awk '/^tx_power/{print $3}' $CONF_FILE)
+distance=$(awk '/^distance/{print $3}' $CONF_FILE)
+coverage_class=$(awk '/^coverage_class/{print $3}' $CONF_FILE)
+
+echo "Govind" $ps_disable $tx_power $distance $coverage_class
+
 #Get lshw
 apt-get install lshw
 
 wifi_driver_list=$(lshw -C network | awk '$1=="configuration:"{print $3}' | awk -F'driver=' '{print $2}')
 echo $wifi_driver_list
-echo "disble ps" "$power_save" "$MESH_IF"
+echo "ps mode:""$ps_disable" "mesh if:""$MESH_IF"
+phy_if=$(iw dev wlp2s0 info | awk '/wiphy/ {printf "phy" $2}')
 for element in $wifi_driver_list;
 do
   case $element in
     "ath10k")
       echo "ath10k driver"
-      if [ "$power_save" = 1 ];
+      if [ -f "$CONF_FILE" ];
       then
-        disable_wifi_powersave
+        apply_radio_config
       fi;;
     "ath11k")
       echo "ath11k driver"
-      if [ "$power_save" = 1 ];
+      if [ -f "$CONF_FILE" ];
       then
-        disable_wifi_powersave
+        apply_radio_config
       fi;;
     "brcmfmac")
       echo "broadcom full mac driver"
-      if [ "$power_save" = 1 ];
+      if [ -f "$CONF_FILE" ];
       then
-        disable_wifi_powersave
+        apply_radio_config
       fi;;
     "rt2800pci")
       echo "realteck pci driver"
-      if [ "$power_save" = "1" ];
+      if [ -f "$CONF_FILE" ];
       then
-        disable_wifi_powersave
+        apply_radio_config
       fi;;
     "rt2800usb")
       echo "realteck usb driver"
-      if [ "$power_save" = "1" ];
+      if [ -f "$CONF_FILE" ];
       then
-        disable_wifi_powersave
+        apply_radio_config
       fi;;
   esac
 done
